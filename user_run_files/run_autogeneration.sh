316eec738aec001050771c19a555e1e0ddc1b1dd
finbar <farg967@aucklanduni.ac.nz>
2021-11-19 11:06:16 +1300

    autogen file without param id made

--- user_run_files/run_autogeneration.sh
+++ user_run_files/run_autogeneration.sh
@@ -1,3 +1,3 @@
 # user inputs are defined in user_inputs.sh
 source user_inputs.sh
-${opencor_pythonshell_path} ../src/scripts/script_generate_with_new_architecture.py ${file_prefix}_vessel_array.csv ${input_param_file} ${param_id_output_dir} ${file_prefix} ${param_id_method}_${file_prefix}
+${opencor_pythonshell_path} ../src/scripts/script_generate_with_new_architecture.py ${file_prefix}_vessel_array.csv ${input_param_file} ${param_id_output_dir} ${file_prefix}
