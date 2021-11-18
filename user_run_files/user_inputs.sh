383f6fd52e72d584b2bbf7d532e59d8a0d436bab
Finbar Argus <farg967@aucklanduni.ac.nz>
2021-11-19 10:35:23 +1300

    NA

--- user_run_files/user_inputs.sh
+++ user_run_files/user_inputs.sh
@@ -4,14 +4,14 @@ input_param_file=parameters_orig.csv # this must be stored in resources
 
 ## parameter identification inputs
 param_id_method=bayesian
-num_procs=1
-num_calls_to_function=100
+num_procs=20
+num_calls_to_function=2000
 param_id_output_dir=../generated_models/ # path is from the user_run_files dir
 
 ## paths
 ## the below for my hpc
-# opencor_pythonshell_path=/people/farg967/.local/share/OpenCOR-2021-07-04-Linux/pythonshell
+opencor_pythonshell_path=/people/farg967/.local/share/OpenCOR-2021-07-04-Linux/pythonshell
 ## the below for my local
-opencor_pythonshell_path=/opt/OpenCOR-2021-10-05-Linux/pythonshell
+# opencor_pythonshell_path=/opt/OpenCOR-2021-10-05-Linux/pythonshell
 ## Users should modify opencor_pythonshell_path to the path of their own opencor pythonshell
 
