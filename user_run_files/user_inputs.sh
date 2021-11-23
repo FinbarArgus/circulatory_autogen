3e814241625473cb85ca8fcbf5086c2b043c232c
Finbar Argus <farg967@aucklanduni.ac.nz>
2021-11-24 08:58:58 +1300

    changes to user_inputs

--- user_run_files/user_inputs.sh
+++ user_run_files/user_inputs.sh
@@ -5,7 +5,7 @@ input_param_file=parameters_orig.csv # this must be stored in resources
 ## parameter identification inputs
 ## param_id_method can be any of [genetic_algorithm, bayesian]
 param_id_method=genetic_algorithm
-num_procs=30
+num_procs=31
 num_calls_to_function=50000
 param_id_output_dir=../generated_models/ # path is from the user_run_files dir
 
