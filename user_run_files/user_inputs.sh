d48a1beb2b044f53b4ae7b35cae4669ec74ce657
finbar <farg967@aucklanduni.ac.nz>
2021-11-24 08:46:20 +1300

    Fixed a bug where plot_param_id.sh called different id parameters than in run_param_id.sh

--- user_run_files/user_inputs.sh
+++ user_run_files/user_inputs.sh
@@ -7,6 +7,9 @@ param_id_method=bayesian
 num_procs=3
 num_calls_to_function=100
 param_id_output_dir=../generated_models/ # path is from the user_run_files dir
+input_params_to_id=True # Whether an input file for params to identify is given in resources
+                        # This should be named {file_prefix}_params_to_id.csv
+                        # If false the params to identify will be chosen automatically.
 
 ## paths
 ## the below for my hpc
