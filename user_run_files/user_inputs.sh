255179b3a8c2fc9973a4afa15c509b4dd8ad68d1
Finbar Argus <farg967@aucklanduni.ac.nz>
2021-11-24 17:56:22 +1300

    ran param id for simple_physiological, all working

--- user_run_files/user_inputs.sh
+++ user_run_files/user_inputs.sh
@@ -5,8 +5,8 @@ input_param_file=parameters_orig.csv # this must be stored in resources
 ## parameter identification inputs
 ## param_id_method can be any of [genetic_algorithm, bayesian]
 param_id_method=genetic_algorithm
-num_procs=3
-num_calls_to_function=500
+num_procs=31
+num_calls_to_function=50000
 param_id_output_dir=../generated_models/ # path is from the user_run_files dir
 input_params_to_id=True # Whether an input file for params to identify is given in resources
                         # This should be named {file_prefix}_params_to_id.csv
@@ -14,8 +14,8 @@ input_params_to_id=True # Whether an input file for params to identify is given 
 
 ## paths
 ## the below for my hpc
-# opencor_pythonshell_path=/people/farg967/.local/share/OpenCOR-2021-07-04-Linux/pythonshell
+opencor_pythonshell_path=/people/farg967/.local/share/OpenCOR-2021-07-04-Linux/pythonshell
 ## the below for my local
-opencor_pythonshell_path=/opt/OpenCOR-2021-10-05-Linux/pythonshell
+# opencor_pythonshell_path=/opt/OpenCOR-2021-10-05-Linux/pythonshell
 ## Users should modify opencor_pythonshell_path to the path of their own opencor pythonshell
 
