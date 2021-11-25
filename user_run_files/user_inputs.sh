283d508d618031451f66b50729cb942a5bc008c2
finbar <farg967@aucklanduni.ac.nz>
2021-11-26 09:25:08 +1300

    na

--- user_run_files/user_inputs.sh
+++ user_run_files/user_inputs.sh
@@ -1,6 +1,6 @@
 ## general inputs
-file_prefix=simple_physiological
-input_param_file=parameters_orig.csv # this must be stored in resources.
+file_prefix=3compartment
+input_param_file=3compartment_parameters.csv # this must be stored in resources.
                                      # If first creating a model
                                      # set this to parameters_orig.csv and a 
                                      # parameters file will be generated with 
@@ -12,7 +12,7 @@ param_id_method=genetic_algorithm
 num_procs=31
 num_calls_to_function=30000
 param_id_output_dir=../generated_models/ # path is from the user_run_files dir
-param_id_obs_file=simple_physiological_obs_data.json
+param_id_obs_file=3compartment_obs_data.json
 input_params_to_id=True # Whether an input file for params to identify is given in resources
                         # This should be named {file_prefix}_params_to_id.csv
                         # If false the params to identify will be chosen automatically.
@@ -20,8 +20,8 @@ input_params_to_id=True # Whether an input file for params to identify is given 
 
 ## paths
 ## the below for my hpc
-opencor_pythonshell_path=/people/farg967/.local/share/OpenCOR-2021-07-04-Linux/pythonshell
+# opencor_pythonshell_path=/people/farg967/.local/share/OpenCOR-2021-07-04-Linux/pythonshell
 ## the below for my local
-# opencor_pythonshell_path=/opt/OpenCOR-2021-10-05-Linux/pythonshell
+opencor_pythonshell_path=/opt/OpenCOR-2021-10-05-Linux/pythonshell
 ## Users should modify opencor_pythonshell_path to the path of their own opencor pythonshell
 
