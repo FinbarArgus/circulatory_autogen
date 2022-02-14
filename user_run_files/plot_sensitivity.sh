b6c6d7cec1dd3a7e591072070b5bab9b3f6957e4
matthew <msavage@slingshot.co.nz>
2022-02-15 10:37:33 +1300

    deleted unused files

--- user_run_files/plot_sensitivity.sh
+++
@@ -1,4 +0,0 @@
-# user inputs are defined in user_inputs.sh
-source user_inputs.sh
-${opencor_pythonshell_path} ../src/scripts/plot_sensitivity_script.py ${param_id_method} ${file_prefix} ${param_id_obs_path}
-
