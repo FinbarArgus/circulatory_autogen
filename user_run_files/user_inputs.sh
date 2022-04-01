3a53e138e2e618b200cc868217e18261c417a328
finbar <farg967@aucklanduni.ac.nz>
2022-04-01 16:12:32 +1300

    na

--- user_run_files/user_inputs.sh
+++ user_run_files/user_inputs.sh
@@ -18,7 +18,7 @@ input_param_file=3compartment_parameters.csv # this must be stored in resources.
 ## param_id_method can be any of [genetic_algorithm, bayesian, mcmc]
 param_id_method=mcmc
 # num_procs=31
-num_procs=2
+num_procs=4
 num_calls_to_function=10000
 num_param_id_runs=1
 run_sensitivity=False
