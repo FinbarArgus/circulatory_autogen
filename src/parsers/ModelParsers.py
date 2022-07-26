517735d4c015eb804ffb0f6218c342032ce15638
finbar <farg967@aucklanduni.ac.nz>
2022-07-27 09:02:29 +1200

    created tests, updated test cases so they are all working with new functionality

--- src/parsers/ModelParsers.py
+++ src/parsers/ModelParsers.py
@@ -145,6 +145,8 @@ class CSV0DModelParser(object):
 
                 all_parameters_defined = False
 
+        if len(set([len(a) for a in parameters_list])) != 1:
+            print('parameters rows are of non consistent length, exiting')
         parameters_array = np.empty((len(parameters_list)),
                                     dtype=[('variable_name', 'U64'), ('units', 'U64'),('const_type', 'U64'),
                                            ('value', 'U64'), ('data_reference', 'U64')])
---
+++ user_run_files/run_test_autogeneration.sh
@@ -0,0 +1,44 @@
+# user inputs are defined in user_inputs.sh
+source user_inputs.sh
+
+echo Running Tests for autogeneration
+echo _______________________________
+
+echo Running 3compartment autogeneration test
+echo _______________________________
+file_prefix=3compartment
+input_param_file=3compartment_parameters.csv # this must be stored in resources.
+${opencor_pythonshell_path} ../src/scripts/script_generate_with_new_architecture.py ${file_prefix}_vessel_array.csv ${input_param_file} ${file_prefix}
+echo 
+echo _______________________________
+echo Running simple_physiological autogeneration test
+echo _______________________________
+file_prefix=simple_physiological
+input_param_file=simple_physiological_parameters.csv # this must be stored in resources.
+${opencor_pythonshell_path} ../src/scripts/script_generate_with_new_architecture.py ${file_prefix}_vessel_array.csv ${input_param_file} ${file_prefix}
+echo 
+echo _______________________________
+echo Running neonatal autogeneration test
+echo _______________________________
+file_prefix=neonatal
+input_param_file=neonatal_parameters.csv # this must be stored in resources.
+${opencor_pythonshell_path} ../src/scripts/script_generate_with_new_architecture.py ${file_prefix}_vessel_array.csv ${input_param_file} ${file_prefix}
+echo 
+echo _______________________________
+echo Running physiological autogeneration with id params test
+echo _______________________________
+file_prefix=physiological
+input_param_file=physiological_parameters.csv # this must be stored in resources.
+param_id_obs_path=/home/finbar/Documents/data/heart_projects/Argus_2022/observables_biobeat_BB153.json 
+${opencor_pythonshell_path} ../src/scripts/script_generate_with_new_architecture.py ${file_prefix}_vessel_array.csv ${input_param_file} ${file_prefix} ${param_id_method} ${param_id_obs_path}
+echo 
+echo _______________________________
+echo Running control_phys autogeneration with id params test
+echo _______________________________
+file_prefix=control_phys
+input_param_file=control_phys_parameters.csv # this must be stored in resources.
+param_id_obs_path=/home/finbar/Documents/data/heart_projects/Argus_2022/observables_biobeat_BB153.json 
+${opencor_pythonshell_path} ../src/scripts/script_generate_with_new_architecture.py ${file_prefix}_vessel_array.csv ${input_param_file} ${file_prefix} ${param_id_method} ${param_id_obs_path}
+echo ______________________________
+echo Testing complete
+echo ______________________________
