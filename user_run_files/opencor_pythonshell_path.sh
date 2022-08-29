ac42c0053f4fa8f0a4f5e0022f47e92a810ca3c6
Behdad Shaarbaf Ebrahimi <behdad.ebrahimi@gmail.com>
2022-08-29 15:34:46 +1200

    lung ROM model vessel array and parameters set

--- user_run_files/opencor_pythonshell_path.sh
+++ user_run_files/opencor_pythonshell_path.sh
@@ -3,5 +3,5 @@
 # the below for my hpc
 # opencor_pythonshell_path: /hpc/farg967/OpenCOR-2022-05-23-Linux/pythonshell
 # the below for my local
-opencor_pythonshell_path=/home/farg967/software/OpenCOR-2022-05-31-Linux/pythonshell
+opencor_pythonshell_path=/hpc/bsha219/Programs/OpenCOR-2022-05-31-Linux/pythonshell
 # opencor_pythonshell_path: /home/ash252/Downloads/OpenCOR-2021-10-05-Linux/pythonshell
