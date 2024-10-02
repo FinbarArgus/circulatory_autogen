33b86ca1d41a4c125c205643f9d82f5f5766fdfa
Finbar Argus <farg967@aucklanduni.ac.nz>
2024-10-02 15:35:38 +1300

    small changes

--- user_run_files/opencor_pythonshell_path.sh
+++ user_run_files/opencor_pythonshell_path.sh
@@ -4,5 +4,5 @@
 # opencor_pythonshell_path=/hpc/farg967/OpenCOR-2022-05-23-Linux/pythonshell
 # opencor_pythonshell_path=/hpc/farg967/OpenCOR-0-7-1-Linux/pythonshell
 # the below for my local
-opencor_pythonshell_path=/home/farg967/software/OpenCOR-0-7-1-Linux/pythonshell
-# opencor_pythonshell_path=/home/farg967/software/OpenCOR-2024-08-23-Linux/pythonshell
+# opencor_pythonshell_path=/home/farg967/software/OpenCOR-0-7-1-Linux/pythonshell
+opencor_pythonshell_path=/home/farg967/software/OpenCOR-2024-08-23-Linux/pythonshell
