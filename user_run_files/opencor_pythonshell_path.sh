cd21c0a299fec81c83c17b4600b1638f99788686
Finbar Argus <farg967@aucklanduni.ac.nz>
2025-01-21 16:29:35 +1300

    updating opencor path

--- user_run_files/opencor_pythonshell_path.sh
+++ user_run_files/opencor_pythonshell_path.sh
@@ -1,13 +1,6 @@
 #_______python paths _________
 # Users should modify opencor_pythonshell_path to the path of their own opencor pythonshell
 # the below for my hpc
-# opencor_pythonshell_path=/hpc/farg967/OpenCOR-2022-05-23-Linux/pythonshell
 # opencor_pythonshell_path=/hpc/farg967/OpenCOR-0-7-1-Linux/pythonshell
 # the below for my local
-<<<<<<< Updated upstream
-# opencor_pythonshell_path=/home/farg967/software/OpenCOR-0-7-1-Linux/pythonshell
-opencor_pythonshell_path=/home/farg967/software/OpenCOR-2024-11-29-Linux/pythonshell
-=======
 opencor_pythonshell_path=/home/farg967/software/OpenCOR-0-8-Linux/pythonshell
-# opencor_pythonshell_path=/home/ash252/Downloads/OpenCOR-2021-10-05-Linux/pythonshell
->>>>>>> Stashed changes
