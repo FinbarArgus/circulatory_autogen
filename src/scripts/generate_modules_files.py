133d0ae1c87302c63427fc7264b963fb0c808c40
Rashmini <rashmini.dkn@gmail.com>
2025-02-08 12:39:24 +1300

    Add vessel_array generation and units extraction

--- src/scripts/generate_modules_files.py
+++ src/scripts/generate_modules_files.py
@@ -1,6 +1,6 @@
 """
 Converts a CellML model to a module and generate files to run Circulatory Autogen.
-Inputs: CellML model and the output directory where files are created
+Inputs: CellML model and the output directory where parameters.csv and vessel_array.csv files are created
 """
 import argparse
 import json
@@ -10,12 +10,27 @@ import xml.etree.ElementTree as ET
 
 import libcellml as lc
 
+# Define namespaces
+cellml_namespace = "http://www.cellml.org/cellml/1.1#"
+mathml_namespace = "http://www.w3.org/1998/Math/MathML"
+
+# Define units file paths
+user_units_cellml = '../module_config_user/user_units.cellml'
+units_cellml = '../src/generators/resources/units.cellml'
+
+# Define file_prefix, vessel_name and data_reference for the model
+file_prefix = "NKE_pump"
+vessel_name = "NKE_system"
+data_reference = "user_defined"
+
+# Parse arguments
 def _parse_args():
     parser = argparse.ArgumentParser(prog="convert_cellml_for_ca")
     parser.add_argument("-i", "--input-model", help="import CellML model to convert.", required=True)
     parser.add_argument("-o", "--output-dir", help="output directory for converted model data.", required=True)
     return parser.parse_args()
 
+# Print errors
 def _print_errors(l):
     print("Total errors: ", l.errorCount())
     for i in range(l.errorCount()):
@@ -43,12 +58,12 @@ def _extract_variables_constants_states(analysed_model):
         analysed_model_state = analysed_model.state(i)
         state = analysed_model_state.variable()
         constants.append([f"{state.name()}_init", state.units().name(), "constant", state.initialValue()])
-        states.append([state.name(), state.units().name()])
+        states.append([state.name(), state.units().name(), "variable"])
 
     return variables, constants, states
 
 # Generate module_config.json file
-def _generate_module_config(output_dir, variables, constants, file_prefix, component_name):
+def _generate_module_config(variables, constants, states, file_prefix, component_name):
     config = {
         "vessel_type": file_prefix,
         "BC_type": "nn",
@@ -70,15 +85,29 @@ def _generate_module_config(output_dir, variables, constants, file_prefix, compo
         info = [constant[0], constant[1], "access", constant[2]]
         config["variables_and_units"].append(info)
 
+    # Add state variables
+    for state in states:
+        info = [state[0], state[1], "access", state[2]]
+        config["variables_and_units"].append(info)
+
     module_config = [config]
 
-    file_path = os.path.join(output_dir, f"{file_prefix}_module_config.json")
+    file_path = os.path.join('../module_config_user', f"{file_prefix}_module_config.json")
 
     with open(file_path, "w") as fh:
         json.dump(module_config, fh, indent=2)
 
+# Generate vessel_array.csv file
+def _generate_vessel_array_csv(output_dir, vessel_name, file_prefix):
+
+    file_path = os.path.join(output_dir, f"{file_prefix}_vessel_array.csv")
+
+    with open(file_path, "w") as fh:
+        fh.writelines(["name,BC_type,vessel_type,inp_vessels,out_vessels\n"])
+        fh.writelines([f"{vessel_name},nn,{file_prefix},,\n"])
+
 # Generate parameters.csv file
-def _generate_parameters_csv(output_dir, constants, file_prefix, data_reference):
+def _generate_parameters_csv(output_dir, constants, vessel_name, file_prefix, data_reference):
 
     file_path = os.path.join(output_dir, f"{file_prefix}_parameters.csv")
 
@@ -86,18 +115,14 @@ def _generate_parameters_csv(output_dir, constants, file_prefix, data_reference)
         fh.writelines(["variable_name,units,value,data_reference\n"])
 
         for constant in constants:
-            fh.writelines([f"{constant[0]}_{file_prefix},{constant[1]},{constant[3]},{data_reference}\n"])
+            fh.writelines([f"{constant[0]}_{vessel_name},{constant[1]},{constant[3]},{data_reference}\n"])
 
 # Generate CellML module
-def _generate_cellml_module(output_dir, input_model, states, file_prefix):
+def _generate_cellml_module(input_model, states, file_prefix):
 
     # Parse the cellml file
     tree = ET.parse(input_model)
     root = tree.getroot()
-
-    # Define namespaces
-    cellml_namespace = "http://www.cellml.org/cellml/1.1#"
-    mathml_namespace = "http://www.w3.org/1998/Math/MathML"
 
     # Add xmlns:cellml to the root model element
     root.set("xmlns:cellml", cellml_namespace)
@@ -106,32 +131,14 @@ def _generate_cellml_module(output_dir, input_model, states, file_prefix):
     ET.register_namespace("", cellml_namespace) 
     ET.register_namespace("math", mathml_namespace)
 
-    # Modify variable elements
-    for variable in root.findall(f".//{{{cellml_namespace}}}variable"):
-        if variable.attrib["name"]=="t":
-            variable.set("public_interface", "in")
-            if "initial_value" in variable.attrib:
-                del variable.attrib["initial_value"]
-        elif "initial_value" in variable.attrib:
-            del variable.attrib["initial_value"]
-            variable.set("public_interface", "in")
-        else:
-            variable.set("public_interface", "out")
+    # Move units to user_units.cellml
+    _update_units_file(root)
 
-    # Modify state variables
-    for state in states:
-        var_name = state[0]
-        units = state[1]
-        
-        variable = root.find(f".//{{{cellml_namespace}}}variable[@name='{var_name}']")
-        
-        if variable is not None:
-            new_name = f"{var_name}_init"
-            variable.set("name", new_name)
-            
-            new_variable = ET.Element(f"{{{cellml_namespace}}}variable", name=var_name, units=units, public_interface="out", initial_value=new_name)
-            
-            root.find(f".//{{{cellml_namespace}}}component").append(new_variable)
+    # Modify variable elements
+    _modify_variables(root)
+
+    # Modify state variable elements
+    _modify_state_variables(root, states)
 
     # Ensure math elements are using the proper namespace
     for math in root.findall(f".//{{{mathml_namespace}}}math"):
@@ -148,9 +155,66 @@ def _generate_cellml_module(output_dir, input_model, states, file_prefix):
     cellml_str = cellml_str.replace('math:', '')
 
     # Write the modified cellml
-    file_path = os.path.join(output_dir, f"{file_prefix}_modules.xml")
+    file_path = os.path.join('../module_config_user', f"{file_prefix}_modules.cellml")
     with open(file_path, "w", encoding="UTF-8") as f:
         f.write(cellml_str)
+
+# Move units to user_units.cellml
+def _update_units_file(root):
+
+    # Extract units from user_units.cellml
+    user_units_tree = ET.parse(user_units_cellml)
+    user_units_root = user_units_tree.getroot()
+    user_units = {unit.get("name"): unit for unit in user_units_root.findall(f".//{{{cellml_namespace}}}units")}
+
+    # Extract units from units.cellml
+    units_tree = ET.parse(units_cellml)
+    units_root = units_tree.getroot()
+    units = {unit.get("name"): unit for unit in units_root.findall(f".//{{{cellml_namespace}}}units")}
+
+    # Extract units from original model
+    units_dict = {unit.get("name"): unit for unit in root.findall(f".//{{{cellml_namespace}}}units")}
+
+
+    # Add units to user_units.cellml
+    for name, unit in units_dict.items():
+        if name not in user_units and name not in units:
+            user_units_root.append(unit)
+
+    # Remove units from the module
+    for unit in root.findall(f".//{{{cellml_namespace}}}units"):
+        root.remove(unit)
+
+    user_units_tree.write(user_units_cellml, xml_declaration=True, encoding="UTF-8")
+
+# Modify variable elements in cellml module
+def _modify_variables(root):
+    for variable in root.findall(f".//{{{cellml_namespace}}}variable"):
+        if variable.attrib["name"]=="t":
+            variable.set("public_interface", "in")
+            if "initial_value" in variable.attrib:
+                del variable.attrib["initial_value"]
+        elif "initial_value" in variable.attrib:
+            del variable.attrib["initial_value"]
+            variable.set("public_interface", "in")
+        else:
+            variable.set("public_interface", "out")
+
+# Modify state variables in cellml module
+def _modify_state_variables(root, states):
+    for state in states:
+        var_name = state[0]
+        units = state[1]
+        
+        variable = root.find(f".//{{{cellml_namespace}}}variable[@name='{var_name}']")
+        
+        if variable is not None:
+            new_name = f"{var_name}_init"
+            variable.set("name", new_name)
+            
+            new_variable = ET.Element(f"{{{cellml_namespace}}}variable", name=var_name, units=units, public_interface="out", initial_value=new_name)
+            
+            root.find(f".//{{{cellml_namespace}}}component").append(new_variable)
 
 def main():
     args = _parse_args()
@@ -194,15 +258,13 @@ def main():
 
     variables, constants, states = _extract_variables_constants_states(analysed_model)
 
-    # Define file_prefix and data_reference
-    file_prefix = "NKE_pump"
-    data_reference = "user_defined"
+    _generate_module_config(variables, constants, states, file_prefix, component.name())
 
-    _generate_module_config(args.output_dir, variables, constants, file_prefix, component.name())
+    _generate_parameters_csv(args.output_dir, constants, vessel_name, file_prefix, data_reference)
 
-    _generate_parameters_csv(args.output_dir, constants, file_prefix, data_reference)
+    _generate_vessel_array_csv(args.output_dir, vessel_name, file_prefix)
 
-    _generate_cellml_module(args.output_dir, args.input_model, states, file_prefix)
+    _generate_cellml_module(args.input_model, states, file_prefix)
 
 if __name__ == "__main__":
     main()
