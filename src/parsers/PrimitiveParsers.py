04bc3b23af19104c0dfd439293296fe50a61922f
Finbar Argus <farg967@aucklanduni.ac.nz>
2025-06-10 12:17:28 +1200

    updating multiple module_config file implementation

--- src/parsers/PrimitiveParsers.py
+++ src/parsers/PrimitiveParsers.py
@@ -316,10 +316,20 @@ class JSONFileParser(object):
         df = pd.DataFrame(json_obj)
         return df
 
-    def json_to_dataframe_with_user_dir(self, json_path, json_dir):
-        df = self.json_to_dataframe(json_path)
-        user_module_dfs = [self.json_to_dataframe(os.path.join(json_dir, file)) \
+    def json_to_dataframe_with_user_dir(self, json_dir, json_user_dir):
+        dfs = [self.json_to_dataframe(os.path.join(json_dir, file)) \
                 for file in os.listdir(json_dir) if file.endswith('.json')]
+        user_module_dfs = [self.json_to_dataframe(os.path.join(json_user_dir, file)) \
+                for file in os.listdir(json_user_dir) if file.endswith('.json')]
+        df = None
+        for json_df in dfs:
+            if df is None:
+                df = json_df
+            else:
+                # concatenate dataframes, ignore index to reset the index
+                # so that it is not duplicated
+                df = pd.concat([df, json_df], ignore_index=True)
+
         for user_module_df in user_module_dfs:
             df = pd.concat([df, user_module_df], ignore_index=True)
         return df
